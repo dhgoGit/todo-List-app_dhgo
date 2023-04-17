9c533f6b9b5b5e456bf0c97774619d050c87886a
dhgoGit <hesher2016@hanmail.net>
2023-04-17 22:20:21 +0900

    [fix] 코틀린 버전 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.6.3"
 	id("io.spring.dependency-management") version "1.1.0"
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
-	kotlin("plugin.jpa") version "1.7.22"
+	kotlin("jvm") version "1.8.10"
+	kotlin("plugin.spring") version "1.8.10"
+	kotlin("plugin.jpa") version "1.8.10"
+
 }
 
 group = "com.example"
